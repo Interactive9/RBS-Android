8fb6ca0c16a498899bf53e4eec35d9a267302c5b
ada10fwi@student.lu.e <Navbarq91>
2015-04-22 20:24:38 +0200

    Ändrat lite i mainActivity och puzzle
    Ändra bara storleken på texten :+1:

--- app/build/generated/source/r/debug/com/Interactive9/RBS/R.java
+++ app/build/generated/source/r/debug/com/Interactive9/RBS/R.java
@@ -19,8 +19,13 @@ public final class R {
         public static final int app_icon=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f070001;
+        public static final int action_settings=0x7f070006;
         public static final int button=0x7f070000;
+        public static final int numberPicker=0x7f070002;
+        public static final int numberPicker2=0x7f070003;
+        public static final int numberPicker3=0x7f070004;
+        public static final int numberPicker4=0x7f070005;
+        public static final int textView=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
